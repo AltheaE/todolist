e4389078ecd1103539deb6a33105f6530a4d8c5e
Althea <althea@metasolutions.se>
2018-06-15 11:03:52 +0200

    Basic structure and

---
+++ resources/js/main.js
@@ -0,0 +1,7 @@
+ const btn= document.getElementById('add-btn').addEventListener('click',function(){
+   console.log("hi");
+   let value = document.getElementById('add-text').value;
+   if (value){
+
+   }
+ });
